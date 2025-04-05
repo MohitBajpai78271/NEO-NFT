daddfe97bc24d5b64d843bd9e8c6c68b04f318a2
Ekansh Bhushan <ekanshbhushan2k22@gmail.com>
2025-04-06 01:30:03 +0530

    ai bot

--- components/NavBar/AIMentor/AIMentor.jsx
+++ components/NavBar/AIMentor/AIMentor.jsx
@@ -54,7 +54,7 @@ const AIMentor = () => {
 
   return (
     <div className={styles.chatContainer}>
-      <div className={styles.chatHeader}>AI Mentor 💬</div>
+      <div className={styles.chatHeader}>BlockBuddy 💬</div>
       <div className={styles.chatBody}>
         {messages.map((msg, idx) => (
           <div
