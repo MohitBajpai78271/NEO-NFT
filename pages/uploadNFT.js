07adc7e1995e0d1c56b8a4a465cb1464e4d2e6cb
Ekansh Bhushan <ekanshbhushan2k22@gmail.com>
2025-04-06 08:01:39 +0530

    upload NFTs

--- pages/uploadNFT.js
+++ pages/uploadNFT.js
@@ -15,20 +15,19 @@ const uploadNFT = () => {
     <div className={Style.uploadNFT}>
       <div className={Style.uploadNFT_box}>
         <div className={Style.uploadNFT_box_heading}>
-          <h1>Create New NFT</h1>
+          <h1>CreateYour NFT</h1>
           <p>
-            You can set preferred display name, create your profile URL and
-            manage other personal settings.
+            You can set preferred display name, create your profile URL, etc.
           </p>
         </div>
 
-        <div className={Style.uploadNFT_box_title}>
+        {/* <div className={Style.uploadNFT_box_title}>
           <h2>Image, Video, Audio, or 3D Model</h2>
           <p>
             File types supported: JPG, PNG, GIF, SVG, MP4, WEBM, MP3, WAV, OGG,
             GLB, GLTF. Max size: 100 MB
           </p>
-        </div>
+        </div> */}
 
         <div className={Style.uploadNFT_box_form}>
           <UploadNFT
